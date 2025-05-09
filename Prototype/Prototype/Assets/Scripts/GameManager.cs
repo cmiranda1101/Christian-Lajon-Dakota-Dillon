913818f198544e321d3c5c963385121e763c6134
dillontouchard <dillon.touchard@yahoo.com>
2025-05-08 22:33:03 -0400

    Updated GameManager Script
    Fixed null reference exception error

--- Prototype/Prototype/Assets/Scripts/GameManager.cs
+++ Prototype/Prototype/Assets/Scripts/GameManager.cs
@@ -43,7 +43,6 @@ public class GameManager : MonoBehaviour
             else if (menuActive == menuPause)
             {
                 StateUnpause();
-                menuActive.SetActive(isPaused);
                 menuActive = null;
             }
         }
