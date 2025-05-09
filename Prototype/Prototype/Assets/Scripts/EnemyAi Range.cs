8159d9dc5419fc2558ae30c28c2346caa85be1b3
lajontechtech <lajonshabazz@gmail.com>
2025-05-09 11:37:14 -0400

    Ranged Enemy Completed
    Ranged Enemy Can fire from the rifle with a bullet. Stopping distance is set to 15.

---
+++ Prototype/Prototype/Assets/Scripts/EnemyAi Range.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyAiRange : MonoBehaviour, IDamage
+{
+
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [SerializeField] int HP;
+    [SerializeField] int facePlayerSpeed;
+
+    [SerializeField] Transform shootingPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] float fireRate;
+
+    Color colorOrig;
+
+    Vector3 playerDir;
+
+    float shootTimer;
+
+    bool playerInRange;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+        GameManager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+        if (playerInRange)
+        {
+            playerDir = (GameManager.instance.player.transform.position - transform.position);
+            agent.SetDestination(GameManager.instance.player.transform.position);
+
+            if (shootTimer >= fireRate)
+            {
+                shoot();
+            }
+            if (agent.remainingDistance<= agent.stoppingDistance)
+            {
+                facePlayer();
+            }
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        agent.SetDestination(GameManager.instance.player.transform.position);
+
+        if (HP <= 0)
+        {
+            GameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.05f);
+        model.material.color = colorOrig;
+    }
+    void facePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * facePlayerSpeed);
+    }
+    void shoot()
+    {
+        shootTimer = 0;
+        Instantiate(bullet, shootingPos.position, transform.rotation);
+    }
+}
--- Prototype/Prototype/Assets/Scripts/GameManager.cs
+++ Prototype/Prototype/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public PlayerController playerScript;
 
-
+    int gameGoalCount;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,6 +23,12 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    public void updateGameGoal(int amount)
+    {
+        gameGoalCount += amount;
+
         
     }
 }
