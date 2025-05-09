cd82c3732a62645983deac3aa4ae24b90925fcd6
dillontouchard <dillon.touchard@yahoo.com>
2025-05-09 17:11:39 -0400

    Update ShopKeeper.cs
    When the player walks in range of the shopkeeper a prompt to shop now appears.

--- Prototype/Prototype/Assets/Scripts/ShopKeeper.cs
+++ Prototype/Prototype/Assets/Scripts/ShopKeeper.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 
 public class ShopKeeper : MonoBehaviour, IInteract
 {
-
+    [SerializeField] GameObject directions;
     public void Interact()
     {
         Debug.Log("Interacting with shopkeeper");
         GameManager.instance.OpenShop();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            directions.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            directions.SetActive(false);
+        }
     }
 
 }
