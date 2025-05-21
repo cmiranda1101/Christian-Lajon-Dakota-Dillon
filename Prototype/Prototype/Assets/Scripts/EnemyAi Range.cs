4872524c3b15387905389dafd2f67062e2937775
lajontechtech <lajonshabazz@gmail.com>
2025-05-21 16:43:41 -0400

    Range AI
    Fov chase and wander all include and update is cleaned up

--- Prototype/Prototype/Assets/Scripts/EnemyAi Range.cs
+++ Prototype/Prototype/Assets/Scripts/EnemyAi Range.cs
@@ -11,30 +11,33 @@ public class EnemyAiRange : MonoBehaviour, IDamage
 
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
-
+    [SerializeField] int FOV;
+    [SerializeField] Transform headPos;
     [SerializeField] int HP;
     [SerializeField] int facePlayerSpeed;
 
     [SerializeField] Transform shootingPos;
     [SerializeField] GameObject bullet;
     [SerializeField] float fireRate;
-    // patrol timer random movement
-    [SerializeField] float patrolRadius = 10f;
-    [SerializeField] float patrolInterval = 5f;
+
+    [SerializeField] float chaseDur;
+    [SerializeField] float forgetDelay;
+    float lastSeen = Mathf.Infinity;
+    bool isChasing = false;
+
+    [SerializeField] float patrolRadius;
+    [SerializeField] float patrolInterval;
     float patrolTimer;
 
     Color colorOrig;
-
     Vector3 playerDir;
 
     [SerializeField] float walkRate;
     float walkTimer;
     float shootTimer;
-    
 
     bool playerInRange;
     bool isMoving;
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -46,38 +49,59 @@ public class EnemyAiRange : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
+        // Increment timers
         shootTimer += Time.deltaTime;
         walkTimer += Time.deltaTime;
         patrolTimer += Time.deltaTime;
 
-        if (playerInRange)
+        // Check if player is in range and can be seen
+        bool canSeePlayer = playerInRange && CanSeePlayer();
+
+        // Handle chasing and movement
+        if (canSeePlayer)
         {
-            isMoving = true;
-            playerDir = (GameManager.instance.player.transform.position - transform.position);
-            agent.SetDestination(GameManager.instance.player.transform.position);
+            StartChasing();
+        }
+        else if (isChasing)
+        {
+            ContinueChasing();
+        }
 
-            if (shootTimer >= fireRate)
-            {
-                shoot();
-                GunShotSound();
-            }
-
-            if (agent.remainingDistance <= agent.stoppingDistance)
-            {
-                isMoving = false;
-                facePlayer();
-            }
-
-            if (walkTimer >= walkRate && isMoving)
-            {
-                WalkSound();
-                walkTimer = 0f;
-            }
+        // Perform actions based on state (chasing or patrolling)
+        if (isChasing)
+        {
+            HandleChase();
         }
         else
         {
             Wander();
         }
+    }
+
+    // Check if the enemy can see the player
+    bool CanSeePlayer()
+    {
+        if (GameManager.instance.player == null || headPos == null)
+            return false;
+
+        Vector3 directionToPlayer = GameManager.instance.player.transform.position - headPos.position;
+        float angleToPlayer = Vector3.Angle(new Vector3(directionToPlayer.x, 0, directionToPlayer.z), transform.forward);
+
+        Debug.DrawRay(headPos.position, directionToPlayer.normalized , Color.red);
+
+        if (angleToPlayer <= FOV)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(headPos.position, directionToPlayer.normalized, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag("Player"))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -87,13 +111,15 @@ public class EnemyAiRange : MonoBehaviour, IDamage
             playerInRange = true;
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            playerInRange = true;
+            playerInRange = false;
         }
     }
+
     public void takeDamage(int amount)
     {
         HP -= amount;
@@ -109,23 +135,76 @@ public class EnemyAiRange : MonoBehaviour, IDamage
             agent.SetDestination(GameManager.instance.player.transform.position);
         }
     }
+
     IEnumerator flashRed()
     {
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.05f);
         model.material.color = colorOrig;
     }
+
+    // Start chasing the player
+    void StartChasing()
+    {
+        isChasing = true;
+        lastSeen = 0f;
+    }
+
+    // Continue chasing the player after losing sight
+    void ContinueChasing()
+    {
+        lastSeen += Time.deltaTime;
+
+        if (lastSeen >= chaseDur)
+        {
+            isChasing = false;
+        }
+    }
+
+    // Handle the actual chasing logic
+    void HandleChase()
+    {
+        isMoving = true;
+        playerDir = GameManager.instance.player.transform.position - transform.position;
+        agent.SetDestination(GameManager.instance.player.transform.position);
+
+        // Shoot at the player if the fire rate allows
+        if (shootTimer >= fireRate)
+        {
+            shoot();
+            GunShotSound();
+        }
+
+        // Stop moving and face the player when close enough
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            isMoving = false;
+            facePlayer();
+        }
+
+        // Play walking sound if moving
+        if (walkTimer >= walkRate && isMoving)
+        {
+            WalkSound();
+            walkTimer = 0f;
+        }
+    }
+
+    // Make the enemy face the player
     void facePlayer()
     {
         Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * facePlayerSpeed);
     }
+
+    // Shoot at the player
     void shoot()
     {
         shootTimer = 0;
         Instantiate(bullet, shootingPos.position, transform.rotation);
     }
 
+    // Play walk sound
     void WalkSound()
     {
         int i = Random.Range(0, walkClips.Length);
@@ -133,12 +212,15 @@ public class EnemyAiRange : MonoBehaviour, IDamage
         walkSource.Play();
     }
 
+    // Play gunshot sound
     void GunShotSound()
     {
         int i = Random.Range(0, gunClips.Length);
         gunSource.clip = gunClips[i];
         gunSource.Play();
     }
+
+    // Randomly choose a position within a given radius
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
         Vector3 randDirection = Random.insideUnitSphere * dist;
@@ -149,6 +231,8 @@ public class EnemyAiRange : MonoBehaviour, IDamage
 
         return navHit.position;
     }
+
+    // Wander when not chasing the player
     void Wander()
     {
         if (agent.remainingDistance <= agent.stoppingDistance && patrolTimer >= patrolInterval)
@@ -159,12 +243,17 @@ public class EnemyAiRange : MonoBehaviour, IDamage
             isMoving = true;
         }
 
+        // Play walk sound if wandering
         if (walkTimer >= walkRate && isMoving)
         {
             WalkSound();
             walkTimer = 0f;
         }
-        Debug.Log("Wandering to: " + agent.destination);
+
+        // Increment lastSeen timer if not chasing
+        if (!isChasing)
+        {
+            lastSeen += Time.deltaTime;
+        }
     }
-    
 }
