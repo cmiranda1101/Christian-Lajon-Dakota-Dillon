2bcc2111f3dc548fe8f8fe8fada60f865e2f9297
cmiranda1101 <139823343+cmiranda1101@users.noreply.github.com>
2025-05-21 15:20:56 -0400

    Refactored gun code to work with scriptable objects

---
+++ Prototype/Prototype/Assets/Scripts/GunStats.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class GunStats : ScriptableObject
+{
+    public GameObject model;
+    public int damage;
+    public float fireRate;
+    public int range;
+    public int currentAmmo;
+    public int magSize;
+    public int magCount;
+    public AudioClip[] shootSounds;
+    public AudioClip reloadSound1;
+    public AudioClip reloadSound2;
+
+    void Start()
+    {
+        currentAmmo = magSize;
+    }
+}
