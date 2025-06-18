160bc214cbcf7936ed84ec37833291e9d7363042
DcentGaming <dabarker2000@yahoo.com>
2025-06-18 15:33:31 -0400

    Added audio feedback for breaking boxes

--- Prototype/Prototype/Assets/Scripts/Bust.cs
+++ Prototype/Prototype/Assets/Scripts/Bust.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 public class Bust : MonoBehaviour
 {
     [SerializeField] Rigidbody[] rb;
+    [SerializeField] AudioClip[] breakClips;
     [SerializeField] int minForce;
     [SerializeField] int maxForce;
     [SerializeField] float destroyTime;
+
+    AudioSource breakSource;
 
     float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        breakSource = gameObject.GetComponent<AudioSource>();
+        int i = Random.Range(0, breakClips.Length);
+        breakSource.clip = breakClips[i];
+        AudioManager.PlaySFX(breakSource, breakClips[i]);
+        
         foreach (Rigidbody rbody in rb) {
             int x = Random.Range(minForce, maxForce);
             int y = Random.Range(minForce, maxForce);
