89c64b8625e83a8c659852cb28364aaa79fa0ae5
DcentGaming <dabarker2000@yahoo.com>
2025-06-28 13:28:11 -0400

    Added effects to breaking hidables

---
+++ Prototype/Prototype/Assets/Scripts/BreakEffect.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class BreakEffect : MonoBehaviour
+{
+    [SerializeField] Rigidbody[] rb;
+    [SerializeField] AudioClip[] breakClips;
+    [SerializeField] int minForce;
+    [SerializeField] int maxForce;
+    [SerializeField] float destroyTime;
+
+    AudioSource breakSource;
+
+    float timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        breakSource = gameObject.GetComponent<AudioSource>();
+        int i = Random.Range(0, breakClips.Length);
+        AudioManager.PlaySFX(breakSource, breakClips[i]);
+        
+        foreach (Rigidbody rbody in rb) {
+            int x = Random.Range(minForce, maxForce);
+            int y = Random.Range(minForce, maxForce);
+            int z = Random.Range(minForce, maxForce);
+            Vector3 randVec = new Vector3(x, y, z);
+            rbody.AddForceAtPosition(randVec, Random.onUnitSphere, ForceMode.Impulse);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= destroyTime) {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
